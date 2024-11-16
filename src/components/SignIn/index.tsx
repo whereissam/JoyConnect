eb602385c9e6878d207e7169b9ed0897ce36ce56
whereissam <sam135642@gmail.com>
2024-11-16 15:30:59 +0700

    style: change sign in button style

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -7,14 +7,24 @@ export const SignIn = () => {
     return (
       <>
         Signed in as {session?.user?.name?.slice(0, 10)} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button
+          className="px-4 py-2 bg-[#58FFA3] text-black rounded-xl w-full mx-5"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
       </>
     );
   } else {
     return (
       <>
         Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
+        <button
+          className="px-4 py-2 bg-[#58FFA3] text-black rounded-xl w-full mx-5"
+          onClick={() => signIn()}
+        >
+          Sign in
+        </button>
       </>
     );
   }
